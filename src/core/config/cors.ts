fb4e36ba784135a5d0ce690b0ca80afd11f58179
Drex72 <femidavooo@gmail.com>
2024-05-01 11:33:45 +0100

    Added options

--- src/core/config/cors.ts
+++ src/core/config/cors.ts
@@ -3,13 +3,13 @@ import { currentOrigin } from "../utils/getCurrentOrigin"
 
 export const allowedOrigins: string | RegExp | (string | RegExp)[] = ["http://localhost:3000", "https://attendance-management-frontend-two.vercel.app"]
 
-const allowedMethods: string[] = ["GET", "POST", "PUT", "DELETE", "PATCH"]
+const allowedMethods: string[] = ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
 
 const allowedHeaders: string[] = ["Content-Type", "Authorization"]
 
 export const corsOptions: CorsOptions = {
     methods: allowedMethods,
     allowedHeaders,
-    origin: "http://localhost:3000",
+    origin: allowedOrigins,
     credentials: true,
 }
