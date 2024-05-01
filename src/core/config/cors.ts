adcb24f8b0c90cecb2c692d7330590c930776dfc
Drex72 <femidavooo@gmail.com>
2024-05-01 11:30:39 +0100

    added new orgin

--- src/core/config/cors.ts
+++ src/core/config/cors.ts
@@ -10,6 +10,6 @@ const allowedHeaders: string[] = ["Content-Type", "Authorization"]
 export const corsOptions: CorsOptions = {
     methods: allowedMethods,
     allowedHeaders,
-    origin: allowedOrigins,
+    origin: "http://localhost:3000",
     credentials: true,
 }
