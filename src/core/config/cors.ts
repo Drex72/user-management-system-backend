a5952d6633a8e3eb494a8e78ce73ce9bd49fa3bc
Drex72 <femidavooo@gmail.com>
2024-05-02 12:07:57 +0100

    test

--- src/core/config/cors.ts
+++ src/core/config/cors.ts
@@ -10,6 +10,6 @@ const allowedHeaders: string[] = ["Content-Type", "Authorization"]
 export const corsOptions: CorsOptions = {
     methods: allowedMethods,
     allowedHeaders,
-    origin: "*",
+    origin: "https://attendance-delta-black.vercel.app",
     // credentials: true,
 }
