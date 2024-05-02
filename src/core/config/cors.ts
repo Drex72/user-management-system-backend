d77c6e007042f3643c20450bd2eb7b2394a965ca
Drex72 <femidavooo@gmail.com>
2024-05-02 11:51:02 +0100

    made changes

--- src/core/config/cors.ts
+++ src/core/config/cors.ts
@@ -1,7 +1,7 @@
 import type { CorsOptions } from "cors"
 import { currentOrigin } from "../utils/getCurrentOrigin"
 
-export const allowedOrigins: string | RegExp | (string | RegExp)[] = ["http://localhost:3000", "https://attendance-management-frontend-two.vercel.app", "https://attendance-delta-black.vercel.app"]
+export const allowedOrigins: string | RegExp | (string | RegExp)[] = ["https://attendance-delta-black.vercel.app"]
 
 const allowedMethods: string[] = ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
 
